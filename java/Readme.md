b32b1f75c776df886cbd3d692457b12fef14bc5c
ching <ching8989889@gmail.com>
2024-10-27 21:21:06 +0800

    Step 5: replace Map<String, List<Task>> tasks with Map<ProjectName, List<Task>>

--- java/Readme.md
+++ java/Readme.md
@@ -2,3 +2,4 @@
 2024/10/27 新增測試內容(新增task錯誤內容、check錯誤內容、help、error訊息)
 2024/10/27 根據Clean Architecture分四層entity、usecase、adapter、io
 2024/10/27 Remove Primitive Obsession:Tasks
+2024/10/27 Remove Primitive Obsession:replace Map<String, List<Task>> tasks with Map<ProjectName, List<Task>>
--- java/src/main/java/com/codurance/training/tasks/entity/Tasks.java
+++ java/src/main/java/com/codurance/training/tasks/entity/Tasks.java
@@ -3,21 +3,21 @@ package com.codurance.training.tasks.entity;
 import java.util.*;
 
 public class Tasks {
-    private final Map<String, List<Task>> tasks;
+    private final Map<ProjectName, List<Task>> tasks;
 
     public Tasks() {
         this.tasks = new LinkedHashMap<>();
     }
 
-    public Set<Map.Entry<String, List<Task>>> entrySet() {
+    public Set<Map.Entry<ProjectName, List<Task>>> entrySet() {
         return tasks.entrySet();
     }
 
-    public void put(String name, ArrayList<Task> tasks) {
-        this.tasks.put(name, new ArrayList<Task>());
+    public void put(ProjectName projectName, ArrayList<Task> tasks) {
+        this.tasks.put(projectName, tasks);
     }
 
-    public List<Task> get(String project) {
-        return tasks.get(project);
+    public List<Task> get(ProjectName projectName) {
+        return tasks.get(projectName);
     }
 }
